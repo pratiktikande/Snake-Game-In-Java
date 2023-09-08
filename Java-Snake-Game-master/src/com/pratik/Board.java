6fe90893ea432840b75f3edfd900ccc6690286b6
unknown <pratiktikande12@gmail.com>
2023-09-08 21:02:53 +0530

    Remaining Commit

--- Java-Snake-Game-master/src/com/pratik/Board.java
+++ Java-Snake-Game-master/src/com/pratik/Board.java
@@ -1,5 +1,4 @@
 package com.pratik;
-
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
--- Java-Snake-Game-master/src/com/pratik/Snake.java
+++ Java-Snake-Game-master/src/com/pratik/Snake.java
@@ -4,7 +4,6 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 
 public class Snake extends JFrame {
-
     public Snake() {
         
         initUI();
